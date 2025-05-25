0038c7dcb550681f54c1703b22ff495dc16f986a
Lucas <lucas.2020109662@unicap.br>
2025-05-24 22:32:09 -0300

    Properties

---
+++ main.lua
@@ -0,0 +1,9 @@
+-- Properties --
+game.Workspace.Baseplate.Material = "Plastic"
+
+local baseplate = game.Workspace.Baseplate
+baseplate.Transparency = 0.5
+baseplate.CastShadow = false
+
+
+-- Functions --
